fd975a9aa2dd1ecbabf9e02c4c4fcc996e3ae10b
vsande <vsande@cimne.upc.edu>
2015-10-29 14:39:35 +0100

    minor fix

--- src/lib/Wrapper/WrapperFactory/I8PWrapperFactory.f90
+++ src/lib/Wrapper/WrapperFactory/I8PWrapperFactory.f90
@@ -169,7 +169,7 @@ contains
                     call Wrapper%Set(Value=Value)
             end select
         endif
-    end subroutine I8PWrapperFactory_Wrap5D
+    end function I8PWrapperFactory_Wrap5D
 
 
     function I8PWrapperFactory_Wrap6D(this, Value) result(Wrapper)
